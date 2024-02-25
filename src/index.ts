91c7fd3247d8f90802aafc591c80ce135857c74b
Volodymyr Machula <machulav@gmail.com>
2024-02-25 18:12:58 +0100

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
-import { PluginDefinition, serve } from "@connery-io/sdk";
-import sendEmail from "./actions/sendEmail.js";
+import { PluginDefinition, serve } from '@connery-io/sdk';
+import sendEmail from './actions/sendEmail.js';
 
 const plugin: PluginDefinition = {
-  title: "Gmail",
-  description: "Gmail plugin for Connery",
+  title: 'Gmail',
+  description: 'Gmail plugin for Connery',
   actions: [sendEmail],
   configurationParameters: [
     {
-      key: "gmailEmailAddress",
-      title: "Gmail email address",
-      description: "Gmail email address to login with and send emails from.",
-      type: "string",
+      key: 'gmailEmailAddress',
+      title: 'Gmail email address',
+      description: 'Gmail email address to login with and send emails from.',
+      type: 'string',
       validation: {
         required: true,
       },
     },
     {
-      key: "gmailAppPassword",
-      title: "Gmail app password",
+      key: 'gmailAppPassword',
+      title: 'Gmail app password',
       description:
-        "Since Gmail does not allow to login with a password, you need to create an app password for your Gmail account. See https://support.google.com/accounts/answer/185833?hl=en for more information.",
-      type: "string",
+        'Since Gmail does not allow to login with a password, you need to create an app password for your Gmail account. See https://support.google.com/accounts/answer/185833?hl=en for more information.',
+      type: 'string',
       validation: {
         required: true,
       },
     },
     {
-      key: "senderName",
-      title: "Sender name",
-      description: "The name of the sender that will appear in the email.",
-      type: "string",
+      key: 'senderName',
+      title: 'Sender name',
+      description: 'The name of the sender that will appear in the email.',
+      type: 'string',
       validation: {
         required: true,
       },
@@ -37,8 +37,8 @@ const plugin: PluginDefinition = {
   ],
   maintainers: [
     {
-      name: "Connery",
-      email: "support@connery.io",
+      name: 'Connery',
+      email: 'support@connery.io',
     },
   ],
 };
