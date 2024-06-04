f04b46cc7f71b0e7e96e1f08574de805eb80c581
Volodymyr Machula <machulav@gmail.com>
2024-06-04 21:19:23 +0200

    CON-135: Adjust plugin to SDK v0.3.0 (#9)
    * CON-135: Adjust plugin to SDK v0.3.0
    
    * chore: remove unused ENV variables
    
    * test: switch connery version to 0.3.0-beta.1 for testing
    
    * chore: bump connery version
    
    * fix: revert changes

--- src/index.ts
+++ src/index.ts
@@ -2,45 +2,9 @@ import { PluginDefinition, setupPluginServer } from 'connery';
 import sendEmail from './actions/sendEmail.js';
 
 const pluginDefinition: PluginDefinition = {
-  title: 'Gmail',
+  name: 'Gmail',
   description: 'A plugin that contains actions to work with Gmail.',
   actions: [sendEmail],
-  configurationParameters: [
-    {
-      key: 'gmailEmailAddress',
-      title: 'Gmail email address',
-      description: 'Gmail email address to login with and send emails from.',
-      type: 'string',
-      validation: {
-        required: true,
-      },
-    },
-    {
-      key: 'gmailAppPassword',
-      title: 'Gmail app password',
-      description:
-        'Since Gmail does not allow to login with a password, you need to create an app password for your Gmail account. See https://support.google.com/accounts/answer/185833?hl=en for more information.',
-      type: 'string',
-      validation: {
-        required: true,
-      },
-    },
-    {
-      key: 'senderName',
-      title: 'Sender name',
-      description: 'The name of the sender that will appear in the email.',
-      type: 'string',
-      validation: {
-        required: true,
-      },
-    },
-  ],
-  maintainers: [
-    {
-      name: 'Connery',
-      email: 'support@connery.io',
-    },
-  ],
 };
 
 const handler = await setupPluginServer(pluginDefinition);
